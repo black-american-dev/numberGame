fc040b4729bc742e5fe07d97d602fcf26f21aeb7
habib <sa27021984@gmail.com>
2026-03-07 01:54:01 +0000

    fix trust proxies middleware

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -8,15 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CheckLogin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next) {
+    public function handle(Request $request, Closure $next)
+    {
         if (!session('user_id')) {
             return redirect('/login');
         }
+
         return $next($request);
     }
 }
