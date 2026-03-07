fe6220b9e2ba71fef9eb13770ffc2d810b19cb99
habib <sa27021984@gmail.com>
2026-03-07 00:11:08 +0000

    change the database to external database

--- docker/start.sh
+++ docker/start.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-echo "Waiting for MySQL to be ready..."
+echo "Waiting for MySQL..."
 
-while ! (echo > /dev/tcp/db/3306) 2>/dev/null; do
-    echo "MySQL not ready yet, retrying in 2 seconds..."
+until mysqladmin ping -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" --silent; do
+    echo "MySQL not ready yet, retrying..."
     sleep 2
 done
 
-echo "MySQL is ready! Waiting 3 more seconds for full init..."
-sleep 3
+echo "MySQL is ready!"
 
-echo "Running migrations..."
 php artisan migrate --force
 
-echo "Starting Apache..."
 apache2-foreground
